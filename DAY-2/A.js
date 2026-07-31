fb64d97e516ec55a528aeb55e9c5193755072ebf
Sarthak Gupta <sarthakguptaaiml0119@gmail.com>
2026-07-31 11:20:02 +0530

    commit

--- DAY-2/A.js
+++ DAY-2/A.js
@@ -3,11 +3,18 @@
 // var text ="SARTHAK GUPTA 2503215300171";
 // console.log(text);
 
-var a=30;
-var b=40;
-if(a>b){
-    console.log("A>B, A=",a);
+// var a=30;
+// var b=40;
+// if(a>b){
+//     console.log("A>B, A=",a);
+// }
+// else{
+//     console.log("B>A, B=",b);
+// }
+
+console.log("=====FUNCTION=====");
+function abes(){
+    console.log("Welcome Students");
+    console.log("Welcome to Node.js");
 }
-else{
-    console.log("B>A, B=",b);
-}
+abes();
