43cd039a209a29041cdb15c4f8d058e109bc329c
Sarthak Gupta <sarthakguptaaiml0119@gmail.com>
2026-08-19 14:15:17 +0530

    array-9

--- DAY-6/array.js
+++ DAY-6/array.js
@@ -78,3 +78,32 @@ console.log("YEAR=",date.getFullYear());
 console.log("MONTH =",date.getMonth()); //SHOWS ONE MONTH BEHIND BECAUSE INDEXING START FROM 0.
 console.log("DATE=",date.getDate());
 console.log("Milliseconds=",date.getMilliseconds());
+
+
+console.log("=====MATH OBJECT=====");
+console.log("pi=",Math.PI);
+
+// Math Constants
+console.log("Math.E (Euler's number):", Math.E);        
+console.log("Math.PI (PI):", Math.PI);       
+console.log("Math.SQRT2 (Square root of 2):", Math.SQRT2);    
+console.log("Math.SQRT1_2 (Square root of 1/2):", Math.SQRT1_2);  
+console.log("Math.LN2 (Natural logarithm of 2):", Math.LN2);      
+console.log("Math.LN10 (Natural logarithm of 10):", Math.LN10);     
+console.log("Math.LOG2E (Base 2 logarithm of E):", Math.LOG2E);    
+console.log("Math.LOG10E (Base 10 logarithm of E):", Math.LOG10E);   
+
+// Math Methods
+console.log("Math.round(4.6) - Rounds to the nearest integer:", Math.round(4.6));
+console.log("Math.ceil(4.9) - Rounds UP to the nearest integer:", Math.ceil(4.9));
+console.log("Math.floor(4.2) - Rounds DOWN to the nearest integer:", Math.floor(4.2));
+console.log("Math.trunc(-4.2) - Returns only the integer part:", Math.trunc(-4.2));
+console.log("Math.sign(-4) - Returns -1, 0, or 1 based on the sign:", Math.sign(-4));
+console.log("Math.pow(8, 2) - Returns 8 to the power of 2:", Math.pow(8, 2));
+console.log("Math.sqrt(64) - Returns the square root of 64:", Math.sqrt(64));
+console.log("Math.abs(-4.7) - Returns the absolute (positive) value:", Math.abs(-4.7));
+console.log("minimum of 10,20,30,40,50 = ",Math.min(10,20,30,40,50));
+
+
+
+
