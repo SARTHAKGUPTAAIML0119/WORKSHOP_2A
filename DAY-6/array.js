fd9c7b353f7ac77a60763ab5459289b90cbec9db
Sarthak Gupta <sarthakguptaaiml0119@gmail.com>
2026-08-19 13:23:42 +0530

    ARRAY

---
+++ DAY-6/array.js
@@ -0,0 +1,15 @@
+console.log("=====STRING OBJECT=====");
+let name="SARTHAK";
+console.log(name);
+
+//STRING LENGTH
+
+console.log("Length =",name.length);
+
+console.log("=====STRING METHODS=====");
+
+//To upper case
+console.log("Upper Case =", name.toUpperCase());
+
+//2. To lower case
+console.log("Lower Case =",name.toLowerCase())
